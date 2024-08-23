11942224d465c9d3fc55fcdd42aed311389c62ea
LtavaresII <30603702+LtavaresII@users.noreply.github.com>
2024-08-23 15:45:45 -0300

    Add files via upload

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -1,3 +1,4 @@
+import airflow
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.operators.bash import BashOperator
@@ -11,16 +12,10 @@ dag = DAG(
     catchup=False,
 )
 
-postgres_connect = BashOperator(
-    task_id='start',
-    bash_command='Invoke-RestMethod -Uri "http://localhost:8083/connectors" -Method Post -Headers @{"Content-Type"="application/json"} -Body (Get-Content -Raw -Path "postgres-source-connector.json")',
-    name ='postgres_connect',
-    dag=dag,
-)
-
 datalake_to_kafka = SparkSubmitOperator(
     task_id='t1',
     application='/opt/bitnami/spark/datalake_to_kafka.py',
+    conn_id='spark-master',
     packages= "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0",
     name='datalake_to_kafka',
     dag=dag,
@@ -29,6 +24,7 @@ datalake_to_kafka = SparkSubmitOperator(
 kafka_to_postgres = SparkSubmitOperator(
     task_id='t2',
     application='/opt/bitnami/spark/kafka_to_postgres.py',
+    conn_id='spark-master',
     packages= "org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,org.postgresql:postgresql:42.6.1",
     name='kafka_to_postgres',
     dag=dag,
@@ -37,6 +33,7 @@ kafka_to_postgres = SparkSubmitOperator(
 postgres_to_kafka = SparkSubmitOperator(
     task_id='t3',
     application='/opt/bitnami/spark/postgres_to_kafka.py',
+    conn_id='spark-master',
     packages = "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.0,org.postgresql:postgresql:42.6.1",
     name='postgres_to_kafka',
     dag=dag,
@@ -45,9 +42,10 @@ postgres_to_kafka = SparkSubmitOperator(
 kafka_to_console = SparkSubmitOperator(
     task_id='t4',
     application='/opt/bitnami/spark/kafka_to_console.py',
+    conn_id='spark-master',
     packages = "org.apache.spark:spark-streaming-kafka-0-10_2.12:3.5.0,org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0",
     name='kafka_to_console',
     dag=dag,
 )
 
-postgres_connect > datalake_to_kafka > kafka_to_postgres > postgres_to_kafka > kafka_to_console
+datalake_to_kafka >> kafka_to_postgres >> postgres_to_kafka >> kafka_to_console
